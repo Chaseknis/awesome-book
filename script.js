e3319c7702d7c59bee4b38107d32d4383cb3d698
EBrain <ebrain.ebrain@gmail.com>
2023-01-09 19:20:52 +0630

    Refactor to class

--- script.js
+++ script.js
@@ -1,37 +1,46 @@
-let books = JSON.parse(localStorage.getItem('books')) ?? [];
-
 const booksEl = document.getElementById('books');
 
-const remove = (e) => {
-  const index = Array.from(booksEl.children).indexOf(e.parentNode);
-  e.parentNode.remove();
-  books = books.filter((_, i) => index !== i);
-  localStorage.setItem('books', JSON.stringify(books));
-};
+class AwesomeBook {
+  constructor() {
+    this.books = JSON.parse(localStorage.getItem('books')) ?? [];
+    if (this.books) [...this.books].reverse().forEach(this.addBook);
+  }
 
-const addBook = (book) => {
-  const bookDiv = document.createElement('div');
-  const titleDiv = document.createElement('div');
-  titleDiv.innerHTML = book.title;
+  remove = (e) => {
+    const index = Array.from(booksEl.children).indexOf(e.parentNode);
+    e.parentNode.remove();
+    this.books = this.books.filter((_, i) => index !== i);
+    localStorage.setItem('books', JSON.stringify(this.books));
+  };
 
-  const authorDiv = document.createElement('div');
-  authorDiv.innerHTML = book.author;
+  addBook = (book) => {
+    const bookDiv = document.createElement('div');
+    const titleDiv = document.createElement('div');
+    titleDiv.innerHTML = book.title;
 
-  const btn = document.createElement('button');
-  btn.innerText = 'Remove';
+    const authorDiv = document.createElement('div');
+    authorDiv.innerHTML = book.author;
 
-  btn.onclick = () => remove(btn);
+    const btn = document.createElement('button');
+    btn.innerText = 'Remove';
 
-  const hr = document.createElement('hr');
+    btn.onclick = () => this.remove(btn);
 
-  bookDiv.append(titleDiv, authorDiv, btn, hr);
+    const hr = document.createElement('hr');
 
-  booksEl.insertAdjacentElement('afterbegin', bookDiv);
-};
+    bookDiv.append(titleDiv, authorDiv, btn, hr);
 
-if (books) [...books].reverse().forEach(addBook);
+    booksEl.insertAdjacentElement('afterbegin', bookDiv);
+  };
+
+  prepend = (book) => {
+    this.books.unshift(book);
+  };
+}
 
 const form = document.querySelector('form');
+const awesomeBook = new AwesomeBook();
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const bookEle = e.target.elements;
@@ -40,8 +49,8 @@ form.addEventListener('submit', (e) => {
 
   const book = { author, title };
 
-  books.unshift(book);
-  addBook(book);
+  awesomeBook.prepend(book);
+  awesomeBook.addBook(book);
 
-  localStorage.setItem('books', JSON.stringify(books));
+  localStorage.setItem('books', JSON.stringify(awesomeBook.books));
 });
